4f8c8d9fa178a0c1f0511f19df6c79ed90519d41
Prasadj21072003 <prasadjadhav2003@gmail.com>
2024-06-04 23:22:50 +0530

    sixth  commit

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -24,9 +24,9 @@ export default function Login() {
     e.preventDefault();
     loginuser(user, dispatch);
 
-    // navigate("/");
+    navigate("/");
 
-    // window.location.reload();
+    //window.location.reload();
   };
 
   return (
